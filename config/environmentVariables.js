b0e7a94ad0a91644dbac8796cb0d56cdbff88ba6
shaileshs <shaileshs@cantahealth.com>
2016-04-05 18:53:35 +0530

    define OR operator

--- config/environmentVariables.js
+++ config/environmentVariables.js
@@ -3,11 +3,11 @@
 // eslint disable no-var
 
 var environmentVariables = {
-  "FHIR_MONGO_CONNECTION_STRING": "mongodb://127.0.0.1:27017/fhir", // "mongodb://127.0.0.1:27017/fhir"
-  "FHIR_LOGGING_LEVEL": "debug", // debug
+  "FHIR_MONGO_CONNECTION_STRING": process.env.FHIR_MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/fhir", // "mongodb://127.0.0.1:27017/fhir"
+  "FHIR_LOGGING_LEVEL": process.env.FHIR_LOGGING_LEVEL || "debug", // debug
   "FHIR_REDIS_HOST": process.env.FHIR_REDIS_HOST,
   "FHIR_REDIS_PORT": process.env.FHIR_REDIS_PORT,
-  "rabbitMQUrl": process.env.rabbitMQUrl // amqp://localhost
+  "rabbitMQUrl": process.env.rabbitMQUrl || "amqp://localhost"
 };
 
 module.exports = environmentVariables;
