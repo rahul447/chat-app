10e85ab46f70ce236955068f7c5bb9ba8a17c6b6
Shakti Mishra <shaktim@cantahealth.com>
2016-08-05 17:33:27 +0530

    version updated & environment variable changed

--- config/environmentVariables.js
+++ config/environmentVariables.js
@@ -5,8 +5,6 @@
 var environmentVariables = {
   "FHIR_MONGO_CONNECTION_STRING": process.env.FHIR_MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/fhir", // "mongodb://127.0.0.1:27017/fhir"
   "FHIR_LOGGING_LEVEL": process.env.FHIR_LOGGING_LEVEL || "debug", // debug
-  "FHIR_REDIS_HOST": process.env.FHIR_REDIS_HOST,
-  "FHIR_REDIS_PORT": process.env.FHIR_REDIS_PORT,
   "FHIR_RABBITMQ_URL": process.env.FHIR_RABBITMQ_URL || "amqp://integration:integration@10.18.6.109:5672",
   "FHIR_QUEUE_NAME": process.env.FHIR_QUEUE_NAME || "test",
   "FHIR_EXCHANGE_NAME": process.env.FHIR_EXCHANGE_NAME || "test-cantaHealth",
